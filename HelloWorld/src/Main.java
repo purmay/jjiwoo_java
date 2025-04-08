702ef850613f9811bbb773c10506005e658cb96b
wldn0829 <k01025058767@gmail.com>
2025-04-08 11:01:17 +0900

    실습문제 추가

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double num1=10.0;
+        final double pi = 3.141592;
+        System.out.println(num1 * num1 * pi);
+    }
+}

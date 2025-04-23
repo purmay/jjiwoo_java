0d3362f5d5c7795c1352dd8194710f3e9b337eb7
wldn0829 <k01025058767@gmail.com>
2025-04-23 22:36:26 +0900

    <중간고사 제출> JAVA프로그래밍1

---
+++ new2/src/GradeCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("점수를 입력하세요: ");
+        int score = scanner.nextInt();
+        String grade;
+
+        if (score >= 90) {
+            grade = "A";
+        }
+        else if (score >= 80) {
+            grade = "B";
+        }
+        else if (score >= 70) {
+            grade = "C";
+        }
+        else if (score >= 60) {
+            grade = "D";
+        }
+        else if (score >= 0) {
+            grade = "F";
+        }
+
+        System.out.println("학점: " + grade);
+        scanner.close();
+    }
+}
